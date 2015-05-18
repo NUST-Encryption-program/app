42425e92f06c5ef3e48e4f8a50e61aee4ac15881
haishuiaa <gaoyanshou1234@126.com>
2015-05-18 22:43:18 +0800

    Update LoginController.java

--- src/main/java/com/app/controllers/LoginController.java
+++ src/main/java/com/app/controllers/LoginController.java
@@ -1,15 +1,16 @@
 package com.app.controllers;
 
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.app.constants.Session;
 import com.app.model.LoginForm;
 import com.app.service.UserService;
 
@@ -22,7 +23,7 @@ public class LoginController {
 
 	@RequestMapping(value="login")
 		
-    public ModelAndView login(HttpServletRequest request,HttpServletResponse response,LoginForm command )
+        public ModelAndView login(HttpServletRequest request,HttpServletResponse response,LoginForm command )
 	{
 		
 		
@@ -36,7 +37,10 @@ public class LoginController {
         	return null;
         }
         
-        userService.insertUser(command);
+        if (userService.insertUser(command) == 0)
+        {
+        	httpSession.setAttribute(Session.CURRENT_USER_ATTR_NAME, command);
+        }
         
         ModelAndView mv = new ModelAndView("/test/index","command","LOGIN SUCCESS, " + username);
         
