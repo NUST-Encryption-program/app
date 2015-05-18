47fba051ae3830e495360d965c87cb78786f9786
haishuiaa <gaoyanshou1234@126.com>
2015-05-18 22:40:29 +0800

    Update DownloadController.java

--- src/main/java/com/app/controllers/DownloadController.java
+++ src/main/java/com/app/controllers/DownloadController.java
@@ -2,10 +2,13 @@ package com.app.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.app.constants.Session;
 
 /**
  * 
@@ -19,13 +22,19 @@ public class DownloadController {
 	
 	@RequestMapping(value="/download/api")
 	 public ModelAndView download(HttpServletRequest request,HttpServletResponse response)
-		{
+	 {
 			
-
+	 	HttpSession session = request.getSession(); 
+		    
+	        if (session.getAttribute(Session.CURRENT_USER_ATTR_NAME) == null)
+	        {  
+	        	//download api	
+	    		 return null;
+	        }
       
 	        ModelAndView mv = new ModelAndView("/test/index","command","download sucessful");
 	        
 	        return mv;
-	    }
+	 }
 		
 }
