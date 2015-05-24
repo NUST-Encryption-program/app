a9308bffb082b02242fe0cebd62a96e5fd606603
haishuiaa <gaoyanshou1234@126.com>
2015-05-24 23:40:02 +0800

    Create FileConstant.java

---
+++ src/main/java/com/app/constants/FileConstant.java
@@ -0,0 +1,30 @@
+package com.app.constants;
+
+public interface FileConstant 
+{
+	String KEY_C_DOWN_FILE = "capi";
+	
+	String KEY_JAVA_DOWN_FILE = "japi";
+	
+	String KEY_REST_DOWN_FILE = "rapi";
+	
+	String KEY_ANDROID_DOWN_FILE = "aapi";
+	
+	String KEY_IPHONE_DOWN_FILE = "iapi";
+	
+	String FILE_NAME_C = "c_api.zip";
+	
+	String FILE_NAME_JAVA = "java_api.zip";
+	
+	String FILE_NAME_REST = "rest_api.zip";
+	
+	String FILE_NAME_ANDROID = "android_app.zip";
+	
+	String FILE_NAME_IPHONE = "iphone_app.zip";
+	
+	//改为可配置
+	String FILE_PATH = "E:\\gaoyanshou";
+	
+	
+
+}
